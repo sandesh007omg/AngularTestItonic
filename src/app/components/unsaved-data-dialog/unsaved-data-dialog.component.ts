c24443da488f3e6d49dfb87ef67e7961a7f0501d
Evolve_08 <eevolve-08@Evolve-08s-MacBook-Pro.local>
2023-12-02 19:39:07 +0545

    styling

--- src/app/components/unsaved-data-dialog/unsaved-data-dialog.component.ts
+++ src/app/components/unsaved-data-dialog/unsaved-data-dialog.component.ts
@@ -4,13 +4,25 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-unsaved-data-dialog',
   template: `
-    <h1 mat-dialog-title>Unsaved Data Detected</h1>
+    <h1 mat-dialog-title>Confirmation Dialog</h1>
     <mat-dialog-content>
       <p>Do you want to load the previously unsaved data?</p>
     </mat-dialog-content>
     <mat-dialog-actions>
-      <button mat-button (click)="dialogRef.close(true)">Yes</button>
-      <button mat-button (click)="dialogRef.close(false)">No</button>
+      <button
+        mat-button
+        (click)="dialogRef.close(true)"
+        class="btn btn-primary"
+      >
+        Yes
+      </button>
+      <button
+        mat-button
+        (click)="dialogRef.close(false)"
+        class="btn btn-primary"
+      >
+        No
+      </button>
     </mat-dialog-actions>
   `,
   styleUrls: ['./unsaved-data-dialog.component.scss'],
